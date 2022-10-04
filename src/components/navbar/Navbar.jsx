e7b949756bad19a5dc8d55383a1fdadafe63be92
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-10-04 18:31:09 +0200

    v.1.2. navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import './navbar.scss'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-
+      <div className='wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageOutlinedIcon className='icon'/>
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlinedIcon className='icon'/>
+          </div>
+          <div className='item'>
+            <FullscreenExitOutlinedIcon className='icon'/>
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlinedIcon className='icon'/>
+            <div className='counter'>1</div>
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className='counter'>2</div>
+          </div>
+          <div className='item'>
+            <FormatListBulletedOutlinedIcon className='icon'/>
+          </div>
+          <div className='item'>
+            <img src='https://avatars.githubusercontent.com/u/91205543?s=400&u=f0a2f68b2ece2f8c41a3c3a4a3e89fae5659ed56&v=4'
+            className='avatar'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
