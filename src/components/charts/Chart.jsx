7e95ffc0cdc7b2fd21dce3527b3ba4b5f2131e6c
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-10-08 18:50:29 +0200

    v.1.5. chart

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './chart.scss'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 
 const Chart = () => {
+
+  const data = [
+    { name: 'April', Total: 1200 },
+    { name: 'May', Total: 2100 },
+    { name: 'June', Total: 1800 },
+    { name: 'July', Total: 1700 },
+    { name: 'August', Total: 1300 },
+    { name: 'September', Total: 1200 },
+  ]
+
+
   return (
     <div className='chart'>
-      Chart
+      <div className='title'>Last 6 months (Revenue)</div>
+      <ResponsiveContainer width='100%' aspect={2 / 1}>
+        <AreaChart width={730} height={250} data={data}
+                   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+          <defs>
+            <linearGradient id='total' x1='0' y1='0' x2='0' y2='1'>
+              <stop offset='5%' stopColor='#8884d8' stopOpacity={0.8} />
+              <stop offset='95%' stopColor='#8884d8' stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey='name' stroke='gray' />
+          {/*<YAxis />*/}
+          <CartesianGrid strokeDasharray='3 3' className='chartGrid' />
+          <Tooltip />
+          <Area type='monotone' dataKey='Total' stroke='#8884d8' fillOpacity={1} fill='url(#total)' />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   )
 }
