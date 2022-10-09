4e825c2765176a1aec0cf9171fe9132fa3a5c5d3
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-10-09 16:40:55 +0200

    v.1.6. table

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../../components/navbar/Navbar'
 import Widget from '../../components/widgets/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/charts/Chart'
+import MyTable from '../../components/table/MyTable'
 
 const Home = () => {
   return (
@@ -21,6 +22,10 @@ const Home = () => {
         <div className='charts'>
           <Featured />
           <Chart />
+        </div>
+        <div className='listContainer'>
+          <div className='listTitle'>Latest Transactions</div>
+          <MyTable />
         </div>
       </div>
     </div>
