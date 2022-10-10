f7f1cf074af2a61a6958e278d307d192fb719e04
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-10-10 17:35:55 +0200

    v.2.1. users.single_page

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './chart.scss'
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 
-const Chart = () => {
+const Chart = ({aspect, title }) => {
 
   const data = [
     { name: 'April', Total: 1200 },
@@ -16,8 +16,8 @@ const Chart = () => {
 
   return (
     <div className='chart'>
-      <div className='title'>Last 6 months (Revenue)</div>
-      <ResponsiveContainer width='100%' aspect={2 / 1}>
+      <div className='title'>{title}</div>
+      <ResponsiveContainer width='100%' aspect={aspect} >
         <AreaChart width={730} height={250} data={data}
                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <defs>
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Chart from '../../components/charts/Chart'
+import List from '../list/List'
+import Datatable from '../../components/datatable/Datatable'
+import MyTable from '../../components/table/MyTable'
 
 const Single = () => {
   return (
-    <div>
-      Single
+    <div className='single'>
+      <Sidebar />
+      <div className='singleContainer'>
+        <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <div className='editButton'>Edit</div>
+            <h1 className='title'>Information</h1>
+            <div className='item'>
+              <img src='https://www.soyuz.ru/public/uploads/files/2/7072143/20170617131019a15ce8dda0.png' alt=''
+                   className='itemImg' />
+              <div className='details'>
+                <h1 className='itemTitle'>Jon Snow</h1>
+                <div className='detailItem'>
+                  <span className='itemKey'>Email: </span>
+                  <span className='itemValue'>jonsnow@gmail.com</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Phone: </span>
+                  <span className='itemValue'>+1 25544788566</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Address: </span>
+                  <span className='itemValue'>Elton st. 234 New York</span>
+                </div>
+                <div className='detailItem'>
+                  <span className='itemKey'>Country: </span>
+                  <span className='itemValue'>USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='right'>
+            <Chart aspect={3 / 1} title='User spending ( Last 6 month )' />
+          </div>
+        </div>
+        <div className='bottom'>
+          <h1 className='title'>Last Transactions</h1>
+          <MyTable />
+        </div>
+      </div>
     </div>
   )
 }
