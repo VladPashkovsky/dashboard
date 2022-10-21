9d84cbe27c08c8f3fd5e149d84befcd7a7838faf
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-10-21 14:44:23 +0200

    v.3.2. firebae_add_data

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 import { fireBaseInfo } from './fireBaseInfo'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: fireBaseInfo.REACT_APP_FIREBASE_KEY,
@@ -12,5 +14,6 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-
+export const db = getFirestore(app)
 export const auth = getAuth()
+export const storage = getStorage(app)
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,12 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './new.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload'
+import { doc, collection, setDoc, addDoc, serverTimestamp } from 'firebase/firestore'
+import { auth, db, storage } from '../../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 const New = ({ inputs, title }) => {
 
   const [file, setFile] = useState('')
+  const [data, setData] = useState({})
+  const [percentage, setPercentage] = useState(null)
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name
+      const storageRef = ref(storage, file.name)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on('state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log('Upload is ' + progress + '% done')
+          setPercentage(progress)
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused')
+              break
+            case 'running':
+              console.log('Upload is running')
+              break
+            default:
+              break
+          }
+        },
+        (error) => {
+          console.log(error)
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setData((prev) => ({ ...prev, img: downloadURL }))
+          })
+        },
+      )
+    }
+    file && uploadFile()
+  }, [file])
+
+  const handleAdd = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await createUserWithEmailAndPassword(auth, data.email, data.password)
+      await setDoc(doc(db, 'users', response.user.uid), {
+        ...data,
+        timeStamp: serverTimestamp(),
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleInput = (e) => {
+    const id = e.target.id
+    const value = e.target.value
+
+    setData({ ...data, [id]: value })
+  }
 
   return (
     <div className='new'>
@@ -27,7 +88,7 @@ const New = ({ inputs, title }) => {
             />
           </div>
           <div className='right'>
-            <form>
+            <form onSubmit={handleAdd}>
               <div className='formInput'>
                 <label htmlFor='file'>
                   Image: <DriveFolderUploadIcon className='icon' />
@@ -43,7 +104,11 @@ const New = ({ inputs, title }) => {
               {inputs.map(input => (
                 <div className='formInput' key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                    id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleInput} />
                 </div>
               ))}
 
@@ -71,7 +136,7 @@ const New = ({ inputs, title }) => {
               {/*  <label>Country</label>*/}
               {/*  <input type='text' placeholder='USA' />*/}
               {/*</div>*/}
-              <button>Send</button>
+              <button disabled={percentage !== null && percentage < 100} type='submit'>Send</button>
             </form>
           </div>
         </div>
