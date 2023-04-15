e73c62dbe9db736b5a23e7fc95bca4e3cc9d9ceb
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-04-15 19:23:36 +0600

    v.6.7. modal product

--- src/components/datafirebase/ProductsDataFirebase.jsx
+++ src/components/datafirebase/ProductsDataFirebase.jsx
@@ -5,9 +5,12 @@ import { productColumnsFirebase } from '../../dataTableSource'
 import { Link } from 'react-router-dom'
 import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase'
+import { Modal } from '../modal/Modal'
 
 const ProductsDataFirebase = ({type}) => {
   const [data, setData] = useState([])
+  const [modalActive, setModalActive] = useState(false)
+  const [currentRowId, setCurrentRowId] = useState(null)
 
   useEffect(() => {
     const unsub = onSnapshot(collection(db, 'products'), (snapShot) => {
@@ -20,10 +23,16 @@ const ProductsDataFirebase = ({type}) => {
     return () => unsub()
   }, [])
 
+  function handleRow(id) {
+    setCurrentRowId(id)
+    setModalActive(true)
+  }
+
   const handleDelete = async (id) => {
     try {
       await deleteDoc(doc(db, 'products', id))
       setData(data.filter(item => item.id !== id))
+      setModalActive(false)
     } catch (err) {
       console.log(err)
     }
@@ -37,7 +46,7 @@ const ProductsDataFirebase = ({type}) => {
             <Link to={`/products/${params.row.id}`} style={{ textDecoration: 'none' }}>
               <div className='viewButton'>View</div>
             </Link>
-            <div className='deleteButton' onClick={() => handleDelete(params.row.id)}>Delete</div>
+            <div className='deleteButton' onClick={() => handleRow(params.row.id)}>Delete</div>
           </div>
         )
       },
@@ -58,7 +67,17 @@ const ProductsDataFirebase = ({type}) => {
                 rowsPerPageOptions={[9]}
                 components={{Toolbar: GridToolbar}}
                 checkboxSelection
+                disableSelectionOnClick
       />
+      <Modal active={modalActive} setActive={setModalActive}>
+        <div className='modalWindow'>
+          <div className='modalText'>Product will be removed</div>
+          <div className='modalButtons'>
+            <button className='modalOk' onClick={() => handleDelete(currentRowId)}>Ok</button>
+            <button className='modalCancel' onClick={() => setModalActive(false)}>Cancel</button>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
