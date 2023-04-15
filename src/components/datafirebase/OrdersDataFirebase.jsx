fd4a16fc9d59be0bda47babe36f9cae0c5456c7c
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-04-15 18:41:42 +0600

    v.6.5. modal orer

--- src/components/datafirebase/OrdersDataFirebase.jsx
+++ src/components/datafirebase/OrdersDataFirebase.jsx
@@ -5,14 +5,14 @@ import { OrdersColumnsFirebase, userColumnsFirebase } from '../../dataTableSourc
 import { Link } from 'react-router-dom'
 import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase'
-import {Modal} from '../modal/Modal'
-import { Button } from '@mui/material'
-
+import ModalOne from '../modalOne/ModalOne'
+import { Modal } from '../modal/Modal'
 
 
 const OrdersDataFirebase = () => {
   const [data, setData] = useState([])
-  const [activeModalNice, setActiveModalNice] = useState(false)
+  const [modalActive, setModalActive] = useState(false)
+  const [currentRowId, setCurrentRowId] = useState(null)
 
   useEffect(() => {
     const unsub = onSnapshot(collection(db, 'orders'), (snapShot) => {
@@ -25,11 +25,16 @@ const OrdersDataFirebase = () => {
     return () => unsub()
   }, [])
 
+  function handleRow(id) {
+    setCurrentRowId(id)
+    setModalActive(true)
+  }
+
   const handleDelete = async (id) => {
     try {
       await deleteDoc(doc(db, 'orders', id))
       setData(data.filter(item => item.id !== id))
-      setActiveModalNice(false)
+      setModalActive(false)
     } catch (err) {
       console.log(err)
     }
@@ -40,7 +45,7 @@ const OrdersDataFirebase = () => {
       field: 'action', headerName: 'Action', width: 200, renderCell: (params) => {
         return (
           <div className='cellAction'>
-            <div className='completeButton' onClick={() => handleDelete(params.row.id)}>Completed</div>
+            <div className='completeButton' onClick={() => handleRow(params.row.id)}>Completed</div>
           </div>
         )
       },
@@ -48,20 +53,32 @@ const OrdersDataFirebase = () => {
   ]
 
   return (
-    <div className='datatable'>
-      <div className='datatableTitle'>
-        Add New Order
-        <Link to='/orders/neworder' className='link' style={{ textDecoration: 'none' }}>Add new</Link>
+      <div className='datatable'>
+        <div className='datatableTitle'>
+          Add New Order
+          <Link to='/orders/neworder' className='link' style={{ textDecoration: 'none' }}>Add new</Link>
+        </div>
+          <DataGrid className='datagrid'
+                    rows={data}
+                    columns={OrdersColumnsFirebase.concat(actionColumn)}
+                    pageSize={9}
+                    rowsPerPageOptions={[9]}
+                    components={{Toolbar: GridToolbar}}
+                    checkboxSelection
+                    disableSelectionOnClick
+
+          />
+        <Modal active={modalActive} setActive={setModalActive}>
+          <div className='modalWindow'>
+            <div className='modalText'>Order will be removed</div>
+            <div className='modalButtons'>
+              <button className='modalOk' onClick={() => handleDelete(currentRowId)}>Ok</button>
+              <button className='modalCancel' onClick={() => setModalActive(false)}>Cancel</button>
+            </div>
+          </div>
+        </Modal>
       </div>
-      <DataGrid className='datagrid'
-                rows={data}
-                columns={OrdersColumnsFirebase.concat(actionColumn)}
-                pageSize={9}
-                rowsPerPageOptions={[9]}
-                components={{Toolbar: GridToolbar}}
-                checkboxSelection
-      />
-    </div>
+
   )
 }
 
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import classes from './modal.module.scss'
+import './modal.scss'
 
 const Modal = ({ active, setActive, children }) => {
-
-  const rootClass = [classes.modal]
-  if (active) {
-    rootClass.push(classes.active)
-  }
-
-  const rootClassContent = [classes.modal__content]
-  if (active) {
-    rootClassContent.push(classes.active)
-  }
-
   return (
-    <div className={rootClass.join(' ')} onClick={() => setActive(false)}>
-      <div className={rootClassContent.join(' ')} onClick={e => e.stopPropagation()}>
+    <div className={active ? 'modal active' : 'modal'} onClick={() => setActive(false)}>
+      <div className={active ? 'modal__content active' : 'modal__content'} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
   )
 }
 
-export {Modal}
+export { Modal }
