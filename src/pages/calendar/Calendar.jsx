a5ebae630eb5e8685301528fd41cae5f99995a48
VladPashkovsky <v.v.pashkovsky@gmail.com>
2023-04-17 23:27:42 +0600

    v.7.1. calendar update

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -10,11 +10,11 @@ import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
 import { Box, colors, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material'
 
+
 const Calendar = ({ title }) => {
-  const theme = useTheme()
   const [currentEvents, setCurrentEvents] = useState([])
 
-  const handleDateClick = (selected) => {
+  const handleEventClick = async (selected) => {
     const title = prompt('Enter a new title for your event')
     const calendarApi = selected.view.calendar
     calendarApi.unselect()
@@ -30,12 +30,13 @@ const Calendar = ({ title }) => {
     }
   }
 
-  const handleEventClick = (selected) => {
+  const handleDateClick = (selected) => {
     const confirm = window.confirm(`The event ${selected.event.title} will be deleted`)
     if (confirm) {
       selected.event.remove()
     }
   }
+
 
   return (
     <div className='calendar'>
@@ -45,49 +46,51 @@ const Calendar = ({ title }) => {
         <div className='topCalendar'>
           <h1>{title}</h1>
         </div>
-        <Box m='20px'>
-          <Box display='flex' justifyContent='space-between'>
-            {/*left part*/}
-            <Box flex='1 1 20%' p='15px' borderRadius='4px'>
-              <Typography variant='h5' color='#999'>Events</Typography>
-              <List>
-                {currentEvents.map((event) => (
-                  <ListItem key={event.id}
-                            sx={{ backgroundColor: colors.lightGreen, margin: '10px 0', borderRadius: '2px' }}>
-                    <ListItemText primary={event.title}
-                                  secondary={<Typography>
-                                    {formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}
-                                  </Typography>} />
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-            {/*right part*/}
-            <Box flex='1 1 100%' ml='15px'>
-              <FullCalendar
-              height='75vh'
-              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-              headerToolbar={{
-                left: 'prev, next today',
-                center: 'title',
-                right: 'dayGridMonth, timeGridWeek, timeGridDay, listMonth'
-              }}
-              initialView='dayGridMonth'
-              editable={true}
-              selectable={true}
-              selectMirror={true}
-              dayMaxEvents={true}
-              select={handleDateClick}
-              eventClick={handleEventClick}
-              eventsSet={(events) => setCurrentEvents(events)}
-              initialEvents={[
-                {id: '1234', title: 'All-day event', date: '2022-04-15'},
-                {id: '4321', title: 'Timed event', date: '2022-02-15'}
-              ]}
-              />
+        <div className='centerCalendar'>
+          <Box m='20px' >
+            <Box display='flex' justifyContent='space-between'>
+              {/*left part*/}
+              <Box flex='1 1 20%' p='15px' borderRadius='4px' >
+                <Typography variant='h5' color='#999'>Events</Typography>
+                <List>
+                  {currentEvents.map((event) => (
+                    <ListItem key={event.id}
+                              sx={{ backgroundColor: 'lightgrey', margin: '10px 0', borderRadius: '2px' }}>
+                      <ListItemText primary={event.title}
+                                    secondary={<Typography>
+                                      {formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}
+                                    </Typography>} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+              {/*right part*/}
+              <Box flex='1 1 100%' ml='15px'>
+                <FullCalendar
+                  height='75vh'
+                  plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+                  headerToolbar={{
+                    left: 'prev next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                  }}
+                  initialView='dayGridMonth'
+                  editable={true}
+                  selectable={true}
+                  selectMirror={true}
+                  dayMaxEvents={true}
+                  select={handleEventClick}
+                  eventClick={handleDateClick}
+                  eventsSet={(events) => setCurrentEvents(events)}
+                  initialEvents={[
+                    {id: '1234', title: 'All-day event', date: '2022-04-15'},
+                    {id: '4321', title: 'Timed event', date: '2022-02-15'}
+                  ]}
+                />
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </div>
       </div>
     </div>
   )
